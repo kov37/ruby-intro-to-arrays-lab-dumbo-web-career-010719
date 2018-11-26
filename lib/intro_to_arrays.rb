796aaaa0ae3a9473f441d166681ca971e176f29d
kovner chalumo <kchalumo@gmail.com>
2018-11-26 23:19:58 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,3 +2,23 @@ def instantiate_new_array
   []
 end 
 
+def array_with_two_elements
+  [1,2]
+end
+
+def first_element(ary)
+  ary[0]
+end
+
+def third_element(ary)
+  ary[2]
+end
+
+def last_element(ary)
+  ary[-1]
+end
+
+def first_element_with_array_methods(ary)
+  ary.shift 
+end
+
